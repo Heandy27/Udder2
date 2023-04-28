9ece4e5a80535655f33cbdc2e325774cf079c5fc
Andy <andy_heredia2008@hotmail.com>
2023-04-28 18:16:50 -0500

    Initial Commit

---
+++ Udder2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder2
+//
+//  Created by Andy Heredia on 28/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
